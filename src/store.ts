6255105beae33ff930c77239f00d16cef71663bc
Alex Lunyov <isuntc@gmail.com>
2019-07-19 21:29:03 +0800

    Revert "Revert "Batch requests"" :D
    This reverts commit 2795c1830e0338b7bdbaed77374cf88153b604a2.

--- src/store.ts
+++ src/store.ts
@@ -13,6 +13,10 @@ const RPC_URL = 'wss://goerli.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65';
 const ENFORCER_ADDR = '0x6949a2a84C9f32C0C905C84686f129a6abC74Ea1';
 const PRIME_CASINO_ADDR = '0xdcd195571746500bf0346e862abb217561a3693b';
 
+type Statuses = { [key: string]: Status };
+type Results = { [key: string]: Result };
+type MyBets = { [key: string]: BigNumber | null };
+
 class Store {
   public web3: Web3;
   public primeCasino: Contract;
@@ -176,35 +180,48 @@ class Store {
     }
   }
 
-  private getResults(taskHash: string, pathRoots: string[]) {
-    return Promise.all(
-      pathRoots.map(pathRoot => {
-        return this.enforcer
-          .getPastEvents('Registered', {
-            fromBlock: 0,
-            filter: {
-              taskHash,
-              solverPathRoot: pathRoot
-            }
-          })
-          .then(([{ returnValues: { solverPathRoot, result } }]) => {
-            return {
+  private getResults(events: EventData[]): Promise<Results> {
+    return this.enforcer
+      .getPastEvents('Registered', {
+        fromBlock: 0,
+        filter: {
+          taskHash: events.map(({ returnValues: { taskHash } }) => taskHash)
+        }
+      })
+      .then(registeredEvents => {
+        return registeredEvents.reduce<Results>(
+          (results, { returnValues: { taskHash, solverPathRoot, result } }) => {
+            results[`${taskHash}-${solverPathRoot}`] = {
               pathRoot: solverPathRoot,
               result
             };
-          });
-      })
-    );
+            return results;
+          },
+          {}
+        );
+      });
   }
 
-  private getStatus(number: BigNumber): Promise<Status> {
-    return this.primeCasino.methods
-      .getStatus(`0x${number.toString(16)}`)
-      .call()
-      .then((status: any) => ({
-        challengeEndTime: status._challengeEndTime,
-        pathRoots: status._pathRoots
-      }));
+  private getStatuses(events: EventData[]): Promise<Statuses> {
+    const batch = new this.web3.BatchRequest();
+    events.forEach(({ returnValues: { number } }) => {
+      const m = this.primeCasino.methods
+        .getStatus(`0x${number.toString(16)}`)
+        .call.request();
+      m.callback = () => null; // stupid web3 will fail without that :facepalm:
+      batch.add(m);
+    });
+    return batch.execute().then((result: any) => {
+      const { response }: { response: any[] } = result;
+      return response.reduce((statuses: Statuses, status: any, i) => {
+        statuses[events[i].returnValues.number.toString(16)] = {
+          challengeEndTime: status._challengeEndTime,
+          pathRoots: status._pathRoots
+        };
+
+        return statuses;
+      }, {});
+    });
   }
 
   private async getMyBets(prime: BigNumber): Promise<BigNumber | null> {
@@ -217,41 +234,91 @@ class Store {
       .call();
   }
 
+  private async getMyBetsBatch(primes: BigNumber[]): Promise<MyBets> {
+    if (!this.address) {
+      return {};
+    }
+
+    const batch = new this.web3.BatchRequest();
+    primes.forEach(prime => {
+      const m = this.primeCasino.methods
+        .getBet(`0x${prime.toString(16)}`, this.address)
+        .call.request();
+      m.callback = () => null;
+      batch.add(m);
+    });
+
+    return batch.execute().then((result: any) => {
+      console.log(result);
+      const { response }: { response: any[] } = result;
+      return response.reduce((myBets, bet, i) => {
+        myBets[primes[i].toString(16)] = bet;
+        return myBets;
+      });
+    });
+  }
+
   @action
   private async addPrimes(events: EventData[]) {
-    const updates: any[] = await Promise.all(
-      events
-        .filter(
-          ({ returnValues: { taskHash } }) =>
-            this.primes.findIndex(p => p.taskHash === taskHash) === -1
-        )
-        .map(
-          async ({
-            returnValues: { number, taskHash, sumYes, sumNo }
-          }): Promise<Prime> => {
-            const status = await this.getStatus(number);
-            const [results, myBets]: [
-              Result[],
-              BigNumber | null
-            ] = await Promise.all([
-              this.getResults(taskHash, status.pathRoots),
-              this.getMyBets(number)
-            ]);
-
-            return {
-              number,
-              taskHash,
-              sumYes,
-              sumNo,
-              status,
-              results,
-              myBets
-            };
-          }
-        )
+    const numbers: BigNumber[] = events.map(
+      ({ returnValues: { number } }) => number
     );
+    Promise.all([this.getStatuses(events), this.getResults(events)]).then(
+      ([statuses, results]) => {
+        this.getMyBetsBatch(numbers).then(myBets => {
+          this.primes.push(
+            ...events.map(
+              ({ returnValues: { number, taskHash, sumYes, sumNo } }) => {
+                const status = statuses[number.toString(16)];
+                const numberResults = status.pathRoots
+                  .map(pathRoot => results[`${taskHash}-${pathRoot}`])
+                  .filter(a => a);
+                return {
+                  number,
+                  taskHash,
+                  sumYes,
+                  sumNo,
+                  status,
+                  results: numberResults,
+                  myBets: myBets[number.toString(16)]
+                };
+              }
+            )
+          );
+        });
+      }
+    );
+    // await Promise.all(
+    //   events
+    //     .filter(
+    //       ({ returnValues: { taskHash } }) =>
+    //         this.primes.findIndex(p => p.taskHash === taskHash) === -1
+    //     )
+    //     .map(
+    //       async ({
+    //         returnValues: { number, taskHash, sumYes, sumNo }
+    //       }): Promise<Prime> => {
+    //         const status = await this.getStatus(number);
+    //         const [results, myBets]: [
+    //           Result[],
+    //           BigNumber | null
+    //         ] = await Promise.all([
+    //           this.getResults(taskHash, status.pathRoots),
+    //           this.getMyBets(number)
+    //         ]);
 
-    this.primes.push(...updates);
+    //         return {
+    //           number,
+    //           taskHash,
+    //           sumYes,
+    //           sumNo,
+    //           status,
+    //           results,
+    //           myBets
+    //         };
+    //       }
+    //     )
+    // );
   }
 
   @action
@@ -272,6 +339,7 @@ class Store {
     for (const {
       returnValues: { solverPathRoot, taskHash, result }
     } of events) {
+      console.log({ solverPathRoot, taskHash, result });
       const index = this.primes.findIndex(prime => prime.taskHash === taskHash);
       this.primes[index].results.push(result);
       this.primes[index].status.pathRoots.push(solverPathRoot);
